01e12da43fde9ac6ff55c4279d1c8fb2f5599c4b
Adam Allison <allisonadam81@gmail.com>
2022-10-06 15:11:21 -0700

    starting styling

--- client/src/components/AddPostItForm/AddPostItForm.tsx
+++ client/src/components/AddPostItForm/AddPostItForm.tsx
@@ -1,4 +1,6 @@
-import React, { useState, memo, FormEvent } from 'react';
+import React, {
+  useState, memo, FormEvent, ChangeEvent, 
+} from 'react';
 import { BoardState, SetBoardState } from '../../types';
 
 type FormProps = {
@@ -16,22 +18,48 @@ export const AddPostItForm = memo(({ setBoardState }: FormProps) => {
   };
 
   return (
-    <div>
+    <div style={{
+      top: '0px',
+      right: '0px',
+      left: '0px',
+      width: '100%',
+      // border: '2px solid black',
+      height: '5rem',
+      backgroundColor: 'lightblue',
+      position: 'fixed',
+    }}
+    >
       <form
+        style={{
+          position: 'absolute', right: '0px', marginRight: '1rem', 
+        }}
         onSubmit={submitHandler}
       >
         <label htmlFor="new-post-it">
           Create Post it!
+          <br />
           <input
+            required
+            style={{
+              marginRight: '.5rem', borderRadius: '1rem', padding: '.5rem', border: 'solid 1px blue', 
+            }}
             type="text"
             name="newHeader"
             id="newHeader"
             value={header}
             placeholder="new post it here..."
-            onChange={(event) => setHeader(event.target.value)}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setHeader(e.target.value)}
           />
         </label>
-        <button type="submit">Create</button>
+        <button
+          type="submit"
+          style={{
+            padding: '.5rem', borderRadius: '1rem', border: 'blue solid 1px', backgroundColor: 'lightblue', 
+          }}
+        >
+          Create
+
+        </button>
       </form>
     </div>
   );
