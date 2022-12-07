b4ba6479262eff0a174a437af22a5c5efe40b266
Adam Allison <allisonadam81@gmail.com>
2022-12-06 17:00:11 -0800

    working select

--- client/Components/FormComponents/InputComponents/SelectInput.tsx
+++ client/Components/FormComponents/InputComponents/SelectInput.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
 
-type Props = {}
+type OptionType = {
+  label: string,
+  value: string
+}
 
-export const SelectInput = (props: Props) => {
+type Props = {
+  HTMLOptions: any,
+  id: string,
+  label: string,
+  name: string,
+  onChange: () => void,
+  options: OptionType[],
+  type: 'text' | 'number',
+  value: string | number,
+  vertical: boolean
+}
+
+export const SelectInput = ({
+  id,
+  label,
+  name,
+  onChange,
+  options,
+  HTMLOptions,
+  value,
+  vertical,
+}: Props) => {
   return (
-    <div>SelectInput</div>
+    <div>
+      <label htmlFor={name}>
+        {label}
+        {vertical ? <br /> : null}
+        <select id={id} name={name} onChange={onChange} onBlur={onChange} value={value} {...HTMLOptions}>
+          <option key="invalid" value="">-- please choose an option --</option>
+          {options.map((option, i) => {
+            return (
+              <option
+                key={`${name} - ${option.value}`}
+                label={option.label}
+                value={option.value}
+              />
+            );
+          })}
+        </select>
+      </label>
+    </div>
   );
 };
